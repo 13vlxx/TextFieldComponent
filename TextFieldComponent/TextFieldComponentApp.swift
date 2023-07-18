caaa67b448cd89100771d739673669b8ec261073
ALX <alexmonac13@gmail.com>
2023-07-18 17:01:22 +0200

    Initial Commit

---
+++ TextFieldComponent/TextFieldComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldComponentApp.swift
+//  TextFieldComponent
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
